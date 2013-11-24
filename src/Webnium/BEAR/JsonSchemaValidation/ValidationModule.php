c38da885f09951c042fd6600c75041f9331c568b
MugeSo <mugeso@mugeso.com>
2013-11-24 15:55:40 +0000

    Add missing ValidationModule

---
+++ src/Webnium/BEAR/JsonSchemaValidation/ValidationModule.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * This file is part of the Webnium BEAR.JsonSchemaValidation package
+ *
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ */
+
+namespace Webnium\BEAR\JsonSchemaValidation;
+
+use Ray\Di\AbstractModule;
+
+/**
+ * Webnium BEAR.JsonSchemaValidation validator module
+ */
+class ValidatorModule extends AbstractModule
+{
+    /**
+     * Configure aspect binding
+     *
+     */
+    protected function cofigure()
+    {
+        $validator = $this->requestInjection(__NAMESPACE__ . '\Interceptor\JsonSchemaValidator');
+        $this->bindInterceptor(
+            $this->matcher->subclassesOf('BEAR\Resource\ResourceObject'),
+            $this->matcher->annotatedWith('BEAR\Sunday\Annotaion\Validate'),
+            [$validator]
+        );
+    }
+}
